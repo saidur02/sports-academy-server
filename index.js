93b25665021f1bb7aab133f258016fc875157e3f
Md Saidur Rahman <saidurrahmanbanguri1997@gmail.com.com>
2023-06-14 16:34:42 +0600

    jwt added

--- index.js
+++ index.js
@@ -64,7 +64,6 @@ async function run() {
 
 
 
-
     app.post('/jwt', (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
