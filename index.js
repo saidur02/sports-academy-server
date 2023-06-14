736aa7d6918c050e60948e0d46db5ff7f47ab656
Md Saidur Rahman <saidurrahmanbanguri1997@gmail.com.com>
2023-06-14 16:33:28 +0600

    class get added

--- index.js
+++ index.js
@@ -66,7 +66,6 @@ async function run() {
 
 
 
-    
     app.post('/jwt', (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
